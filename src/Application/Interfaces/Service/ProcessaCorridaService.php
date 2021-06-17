e7d9f8f780b4cfb6df5d8f2943602efb66191350
samuel_trindade <samuel_trindadexd@hotmail.com>
2021-06-17 13:48:19 -0300

    Adaptando projeto para framework Lumen v8.0

--- src/Application/Interfaces/Service/ProcessaCorridaService.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Application\Interfaces\Service;
-
-interface ProcessaCorridaService
-{
-
-}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,78 +1,16 @@
 <?php
-declare(strict_types=1);
 
-namespace Tests;
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
-use DI\ContainerBuilder;
-use Exception;
-use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\App;
-use Slim\Factory\AppFactory;
-use Slim\Psr7\Factory\StreamFactory;
-use Slim\Psr7\Headers;
-use Slim\Psr7\Request as SlimRequest;
-use Slim\Psr7\Uri;
-
-class TestCase extends PHPUnit_TestCase
+abstract class TestCase extends BaseTestCase
 {
     /**
-     * @return App
-     * @throws Exception
+     * Creates the application.
+     *
+     * @return \Laravel\Lumen\Application
      */
-    protected function getAppInstance(): App
+    public function createApplication()
     {
-        // Instantiate PHP-DI ContainerBuilder
-        $containerBuilder = new ContainerBuilder();
-
-        // Container intentionally not compiled for tests.
-
-        // Set up dependencies
-        $dependencies = require __DIR__ . '/../app/dependencies.php';
-        $dependencies($containerBuilder);
-
-        // Build PHP-DI Container instance
-        $container = $containerBuilder->build();
-
-        // Instantiate the app
-        AppFactory::setContainer($container);
-        $app = AppFactory::create();
-
-        // Register middleware
-        $middleware = require __DIR__ . '/../app/middleware.php';
-        $middleware($app);
-
-        // Register routes
-        $routes = require __DIR__ . '/../app/routes.php';
-        $routes($app);
-
-        return $app;
-    }
-
-    /**
-     * @param string $method
-     * @param string $path
-     * @param array  $headers
-     * @param array  $serverParams
-     * @param array  $cookies
-     * @return Request
-     */
-    protected function createRequest(
-        string $method,
-        string $path,
-        array $headers = ['HTTP_ACCEPT' => 'application/json'],
-        array $serverParams = [],
-        array $cookies = []
-    ): Request {
-        $uri = new Uri('', '', 80, $path);
-        $handle = fopen('php://temp', 'w+');
-        $stream = (new StreamFactory())->createStreamFromResource($handle);
-
-        $h = new Headers();
-        foreach ($headers as $name => $value) {
-            $h->addHeader($name, $value);
-        }
-
-        return new SlimRequest($method, $uri, $h, $cookies, $serverParams, $stream);
+        return require __DIR__.'/../bootstrap/app.php';
     }
 }
